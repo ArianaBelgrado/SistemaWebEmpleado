4fb9a3c47c003068a2b3559cae75a7eaf459d5fd
Ariana Belgrado <belgradoariana@gmail.com>
2023-01-31 17:22:18 -0300

    Agregar archivos de proyecto.

---
+++ SistemaWebEmpleado/Migrations/20230131202024_uno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SistemaWebEmpleado.Migrations
+{
+    public partial class uno : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Empleado",
+                columns: table => new
+                {
+                    EmpleadoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Apellido = table.Column<string>(type: "varchar(50)", nullable: false),
+                    DNI = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Legajo = table.Column<string>(nullable: true),
+                    Titulo = table.Column<string>(type: "varchar(50)", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empleado", x => x.EmpleadoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Empleado");
+        }
+    }
+}
---
+++ SistemaWebEmpleado/Models/Empleado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaWebEmpleado.Models
+{
+    [Table("Empleado")]
+    public class Empleado
+    {
+        public int EmpleadoId { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [Required(ErrorMessage = "Debe ingresar el Nombre")]
+        public string Nombre { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [Required(ErrorMessage = "Debe ingresar el Apellido")]
+        public string Apellido { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [Required(ErrorMessage = "Debe ingresar el DNI")]
+        public string DNI { get; set; }
+
+        [RegularExpression(@"^[A-Z]{2}[1-9]{5}$", ErrorMessage = "Solo se aceptan dos letras y 5 numeros")]
+        public string Legajo { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        [Required(ErrorMessage = "Debe ingresar el Titulo")]
+        public string Titulo { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+
+    }
+}
